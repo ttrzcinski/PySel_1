2c34083cf77f1050d3845d9d00d43851f3c5cd1c
TTrzcinski <trzcinski.tomasz.1988@gmail.com>
2023-10-03 22:34:38 +0200

    Added actual assertion.

--- first_selenium_module/test_public_urls.py
+++ first_selenium_module/test_public_urls.py
@@ -2,11 +2,16 @@ from selenium import webdriver
 
 
 def test_google_pl():
+    # Arrange
     # Get the driver
     driver = webdriver.Chrome()
     # Maximize that Chrome
     driver.maximize_window()
+
+    # Act
     # Get url response
     driver.get("https://www.google.pl/")
+
+    # Assert
     # Check title
-    print("The tile is: ", driver.title)
+    assert(driver.title is not None)
