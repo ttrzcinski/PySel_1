3a49bb380774f9df320844aa84a569a45f55e68e
TTrzcinski <trzcinski.tomasz.1988@gmail.com>
2023-10-05 22:16:04 +0200

    Fixed class filename with cap word convention.

--- shop_in_test_online/test_shops_functionality.py
+++ shop_in_test_online/test_shops_functionality.py
@@ -3,11 +3,14 @@ from selenium.webdriver.common.by import By
 import unittest
 
 
-class Test_Shops_Functionality(unittest.TestCase):
+class TestShopsFunctionality(unittest.TestCase):
+
     def test_finding_iphone_phones(self):
+        # Given
         da_url = "https://ecommerce-playground.lambdatest.io/"
         da_searchbar_textfield = "//input[@name='search']"
         da_searchbar_button = "//button[@type='submit']"
+        da_first_iphone = "//*/a[text()='iPhone']"
 
         test_phrase = "iphone"
 
@@ -15,12 +18,14 @@ class Test_Shops_Functionality(unittest.TestCase):
         driver.maximize_window()
         driver.get(da_url)
 
-        driver.find_element(By.XPATH, da_searchbar_textfield).click()
-        driver.find_element(By.XPATH, da_searchbar_textfield).send_keys(test_phrase)
+        # When
+        da_gui_searchbar = driver.find_element(By.XPATH, da_searchbar_textfield)
+        da_gui_searchbar.click()
+        da_gui_searchbar.send_keys(test_phrase)
         driver.find_element(By.XPATH, da_searchbar_button).click()
 
-        actual_presence = driver.find_element(By.XPATH, "//*/a[text()='iPhone']").is_displayed()
-
+        # Then
+        actual_presence = driver.find_element(By.XPATH, da_first_iphone).is_displayed()
         self.assertTrue(actual_presence)
 
 
